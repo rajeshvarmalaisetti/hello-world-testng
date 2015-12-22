d1f3aeb3db100ad4de08edbdcc5bd54d7a846964
rajesh.laisetti <rajesh.laisetti@SON15930.SONATA.LOCAL>
2015-12-22 12:12:30 +0530

    test case Updated

---
+++ src/main/java/SimpleMaven/SimpleMavenNew/App.java
@@ -0,0 +1,13 @@
+package SimpleMaven.SimpleMavenNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
